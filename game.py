658e1cbb340fd382debea0b1a82e888e9f2dec0d
aprzn123 <noahpberman@gmail.com>
2018-12-20 18:12:36 -0500

    initializes, creates empty window

--- game.py
+++ game.py
@@ -1,1 +1,21 @@
+"""
+The main game.
+"""
 import pygame
+
+
+pygame.init()
+
+WINDOW = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("game")
+
+x = 50
+y = 50
+w = 40
+h = 60
+v = 5
+
+run = True
+while run:
+    pygame.time.delay(100)
