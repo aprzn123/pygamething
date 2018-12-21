52e305c827fd8eeee1aff50dd446d3dfeb82c28d
aprzn123 <noahpberman@gmail.com>
2018-12-20 19:58:18 -0500

    now it moves and escape closes it

--- game.py
+++ game.py
@@ -4,9 +4,12 @@ The main game.
 import pygame
 
 
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
+
 pygame.init()
 
-WINDOW = pygame.display.set_mode((500, 500))
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 pygame.display.set_caption("game")
 
@@ -18,4 +21,26 @@ v = 5
 
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(15)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and x > v:
+        x -= v
+    if keys[pygame.K_RIGHT] and x + w < 500 - v:
+        x += v
+    if keys[pygame.K_UP] and y > v:
+        y -= v
+    if keys[pygame.K_DOWN] and y + h < 500 - v:
+        y += v
+
+    WINDOW.fill((0, 0, 0))
+    pygame.draw.rect(WINDOW, (255, 127, 0), (x, y, w, h))
+
+    pygame.display.update()
+
+pygame.quit()
